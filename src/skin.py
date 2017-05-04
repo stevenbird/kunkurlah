4d8b0fc2b37f2b6b9a8fc5e4b59331d52a61bb69
Steven Bird <stevenbird1@gmail.com>
2017-05-04 19:17:27 +0930

    second crack at it

--- src/skin.py
+++ src/skin.py
@@ -2,209 +2,103 @@
 # Date: 2017-05-03
 
 """
-Bininj Kunwok skin database and functions.
+Bininj Kunwok skin database.
 """
 
-from collections import defaultdict, namedtuple
+# Skins are encoded as matrimoiety (left, right), gender, and generation number.
 
-# Declare all skins (to avoid typos)
-
-NAKAMARRANG = 'nakamarrang'
-NAWAMUD = 'nawamud'
-NANGARRIDJ = 'nangarridj'
-NABULANJ = 'nabulanj'
-
-NAKANGILA = 'nakangila'
-NAWAKADJ = 'nawakadj'
-NABANGARDI = 'nabangardi'
-NAKODJOK = 'nakodjok'
-
-NGALKAMARRANG = 'ngalkamarrang'
-NGALWAMUD = 'ngalwamud'
-NGALNGARRIDJ = 'ngalngarridj'
-NGALBULANJ = 'ngalbulanj'
-
-NGALKANGILA = 'ngalkangila'
-NGALWAKADJ = 'ngalwakadj'
-NGALBANGARDI = 'ngalbangardi'
-NGALKODJOK = 'ngalkodjok'
-
-BANGARDI = 'bangardi'
-KODJOK = 'kodjok'
-BALANG = 'balang'
-KELA = 'kela'
-
-BULANJ = 'bulanj'
-NGARRIDJ = 'ngarridj'
-KAMARRANG = 'kamarrang'
-WAMUD = 'wamud'
-
-BANGARDIDJAN = 'bangardidjan'
-KODJAN = 'kodjan'
-BERLINJDJAN = 'berlinjdjan'
-KALIDJAN = 'kalidjan'
-
-BULANJDJAN = 'bulanjdjan'
-NGARRIDJAN = 'ngarridjan'
-KAMANJDJAN = 'kamanjdjan'
-WAMUDDJAN = 'wamuddjan'
-
-# Attributes (values are arbitrary, not seen externally)
-
-GENDER = 'gender'
 MALE = 'm'
 FEMALE = 'f'
-PATRIMOIETY = 'patrimoiety'
-YIRR = 'y'
-DUWA = 'd'
-MATRIMOIETY = 'matrimoiety'
+
 LEFT = 'l'
 RIGHT = 'r'
-LOCATION = 'location'
-EAST = 'e'
-WEST = 'w'
 
-Skin = namedtuple('Skin', (GENDER, PATRIMOIETY, MATRIMOIETY, LOCATION))
-
-flip = {
-    MALE: FEMALE,
-    FEMALE: MALE,
-    YIRR: DUWA,
-    DUWA: YIRR,
-    LEFT: RIGHT,
-    RIGHT: LEFT,
-    EAST: WEST,
-    WEST: EAST
-}
-    
-attributes = {
-    NAKAMARRANG:   Skin(MALE, YIRR, LEFT, WEST),
-    NAWAMUD:       Skin(MALE, YIRR, LEFT, WEST),
-    NANGARRIDJ:    Skin(MALE, DUWA, LEFT, WEST),
-    NABULANJ:      Skin(MALE, DUWA, LEFT, WEST),
-
-    NAKANGILA:     Skin(MALE, YIRR, RIGHT, WEST),
-    NAWAKADJ:      Skin(MALE, YIRR, RIGHT, WEST),
-    NABANGARDI:    Skin(MALE, DUWA, RIGHT, WEST),
-    NAKODJOK:      Skin(MALE, DUWA, RIGHT, WEST),
-
-    NGALKAMARRANG: Skin(FEMALE, YIRR, LEFT, WEST),
-    NGALWAMUD:     Skin(FEMALE, YIRR, LEFT, WEST),
-    NGALNGARRIDJ:  Skin(FEMALE, DUWA, LEFT, WEST),
-    NGALBULANJ:    Skin(FEMALE, DUWA, LEFT, WEST),
-
-    NGALKANGILA:   Skin(FEMALE, YIRR, RIGHT, WEST),
-    NGALWAKADJ:    Skin(FEMALE, YIRR, RIGHT, WEST),
-    NGALBANGARDI:  Skin(FEMALE, DUWA, RIGHT, WEST),
-    NGALKODJOK:    Skin(FEMALE, DUWA, RIGHT, WEST),
-
-    BANGARDI:      Skin(MALE, YIRR, LEFT, EAST),
-    KODJOK:        Skin(MALE, YIRR, LEFT, EAST),
-    BALANG:        Skin(MALE, DUWA, LEFT, EAST),
-    KELA:          Skin(MALE, DUWA, LEFT, EAST),
-
-    BULANJ:        Skin(MALE, YIRR, RIGHT, EAST),
-    NGARRIDJ:      Skin(MALE, YIRR, RIGHT, EAST),
-    KAMARRANG:     Skin(MALE, DUWA, RIGHT, EAST),
-    WAMUD:         Skin(MALE, DUWA, RIGHT, EAST),
-
-    BANGARDIDJAN:  Skin(FEMALE, YIRR, LEFT, EAST),
-    KODJAN:        Skin(FEMALE, YIRR, LEFT, EAST),
-    BERLINJDJAN:   Skin(FEMALE, DUWA, LEFT, EAST),
-    KALIDJAN:      Skin(FEMALE, DUWA, LEFT, EAST),
-
-    BULANJDJAN:    Skin(FEMALE, YIRR, RIGHT, EAST),
-    NGARRIDJAN:    Skin(FEMALE, YIRR, RIGHT, EAST),
-    KAMANJDJAN:    Skin(FEMALE, DUWA, RIGHT, EAST),
-    WAMUDDJAN:     Skin(FEMALE, DUWA, RIGHT, EAST),
+west = {
+    'lm1': 'nabulanj',         'lf1': 'ngalbulanj',
+    'lm2': 'nawamud',          'lf2': 'ngalwamud',
+    'lm3': 'nangarridj',       'lf3': 'ngalngarridj',
+    'lm4': 'nakamarrang',      'lf4': 'ngalkamarrang',
+    'rm1': 'nawakadj',         'rf1': 'ngalwakadj',
+    'rm2': 'nabangardi',       'rf2': 'ngalbangardi',
+    'rm3': 'nakangila',        'rf3': 'ngalkangila',
+    'rm4': 'nakodjok',         'rf4': 'ngalkodjok'
 }
 
-skins = list(attributes.keys())
-
-lookup = defaultdict(list)
-for skin, attr in attributes.items():
-    lookup[attr].append(skin)
-
-# Relationships
-
-# These are not modelled, just memorised
-karrang = {
-    NAKAMARRANG:   NGALNGARRIDJ,
-    NAWAMUD:       NGALBULANJ,
-    NANGARRIDJ:    NGALWAMUD,
-    NABULANJ:      NGALKAMARRANG,
-
-    NAKANGILA:     NGALBANGARDI,
-    NAWAKADJ:      NGALKODJOK,
-    NABANGARDI:    NGALWAKADJ,
-    NAKODJOK:      NGALKANGILA,
-
-    NGALKAMARRANG: NGALNGARRIDJ,
-    NGALWAMUD:     NGALBULANJ,
-    NGALNGARRIDJ:  NGALWAMUD,
-    NGALBULANJ:    NGALKAMARRANG,
-
-    NGALKANGILA:   NGALBANGARDI,
-    NGALWAKADJ:    NGALKODJOK,
-    NGALBANGARDI:  NGALWAKADJ,
-    NGALKODJOK:    NGALKANGILA,
-
-    BANGARDI:      BERLINJDJAN,
-    KODJAN:        KALIDJAN,
-    BALANG:        KODJOK,
-    KELA:          BANGARDIDJAN,
-
-    BULANJ:        KAMANJDJAN,
-    NGARRIDJ:      WAMUDDJAN,
-    KAMARRANG:     NGARRIDJAN,
-    WAMUD:         BANGARDIDJAN,
-
-    BANGARDIDJAN:  BERLINJDJAN,
-    KODJOK:        KALIDJAN,
-    BERLINJDJAN:   KODJOK,
-    KALIDJAN:      BANGARDIDJAN,
-
-    BULANJDJAN:    KAMANJDJAN,
-    NGARRIDJAN:    WAMUDDJAN,
-    KAMANJDJAN:    NGARRIDJAN,
-    WAMUDDJAN:     BANGARDIDJAN,
+east = {
+    'lm1': 'kela',             'lf1': 'kalidjan',
+    'lm2': 'kodjok',           'lf2': 'kodjdjan',
+    'lm3': 'balang',           'lf3': 'belinj',
+    'lm4': 'bangardi',         'lf4': 'bangardidjan',
+    'rm1': 'ngarridj',         'rf1': 'ngarridjdjan',
+    'rm2': 'kamarrang',        'rf2': 'kamanj',
+    'rm3': 'bulanj',           'rf3': 'bulanjdjan',
+    'rm4': 'wamud',            'rf4': 'wamuddjan'
 }
 
-djedje = defaultdict(list)
-for child, mother in karrang.items():
-    djedje[mother].append(child)
-djedje = dict(djedje)
+increase = {'1': '2', '2': '3', '3': '4', '4': '1'}
+decrease = {'1': '4', '2': '1', '3': '2', '4': '3'}
 
-# standard kinship relationships
-def f(skin): return attributes[skin][GENDER] == FEMALE
-def m(skin): return attributes[skin][GENDER] == MALE
+# utility function
+def p(skin, f):
+    return f(*tuple(skin))
 
-def M(skin): return karrang[skin]
-def fD(skin): ...
+# gender tests
+def f(skin):
+    return p(skin, lambda m, g, n: g == FEMALE)
 
-# need sister/brother pairs
-def S(skin):
-def Z(skin):
+# gender tests
+def m(skin):
+    return p(skin, lambda m, g, n: g == MALE)
 
-ngabba = {}
-kakkali = {}
-travel = {}
-kakkak = {}
-doydoy = {}
-berluh = {}
-ngadjadj = {}
+# duwa?
+def duwa(skin):
+    return p(skin, lambda m, g, n: n in '13')
 
-for skin in skins:
-    gender, patrimoiety, matrimoiety, location = attributes[skin]
-    ngabba[skin] = lookup[Skin(MALE, patrimoiety, flip[matrimoiety], location)]
-    berluh[skin] = lookup[Skin(FEMALE, patrimoiety, flip[matrimoiety], location)]
-    kakkali[skin] = lookup[Skin(flip[gender], flip[patrimoiety], flip[matrimoiety], location)]
-    travel[skin] = lookup[Skin(gender, patrimoiety, matrimoiety, flip[location])]
-    ngadjadj[skin] = lookup[Skin(MALE, flip[patrimoiety], matrimoiety, location)]
-    kakkak[skin] = karrang[karrang[skin]]
-    doydoy[skin] = karrang[karrang[karrang[skin]]]
+# find the mother
+def M(skin):
+    return p(skin, lambda m, _, n: m + FEMALE + decrease[n])
 
-    siblings = djedje[karrang[skin]]
+# find a woman's daughter
+def fD(skin):
+    assert f(skin)
+    return p(skin, lambda m, _, n: m + FEMALE + increase[n])
 
-    if gender == MALE:
-        kankinj[skin] = 
+# brother
+def B1(skin):
+    return p(skin, lambda m, _, n: m + MALE + n)
+def B2(skin):
+    return p(skin, lambda m, _, n: m + MALE + increase[increase[n]])
+
+# sister
+def Z1(skin):
+    return p(skin, lambda m, _, n: m + FEMALE + n)
+def Z2(skin):
+    return p(skin, lambda m, _, n: m + FEMALE + increase[increase[n]])
+
+# spouse
+# NB we're not modelling first and second choices here
+def SP1(skin):
+    return p(skin,
+             lambda m, g, n: (
+                 {LEFT: RIGHT, RIGHT: LEFT}[m] +
+                 {MALE: FEMALE, FEMALE: MALE}[g] +
+                 n))
+
+def SP2(skin):
+    skin = SP1(skin)
+    return p(skin,
+             lambda m, g, n: m + g + increase[increase[n]])
+
+def F1(skin):
+    return P1(M(skin))
+
+def F2(skin):
+    return P2(M(skin))
+
+def mD1(skin):
+    assert m(skin)
+    return fD(SP1(skin))
+
+def mD2(skin):
+    assert m(skin)
+    return fD(SP2(skin))
